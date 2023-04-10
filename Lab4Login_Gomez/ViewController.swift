ff0a54c003f0742cb1215204134c4a89e7b851e4
Byron Gomez Jr <byrongomezjr@protonmail.com>
2023-04-10 18:43:00 -0400

    use of segues and stacks for login functionality.

--- Lab4Login_Gomez/ViewController.swift
+++ Lab4Login_Gomez/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var usernameTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = usernameTextField.text
+    }
 }
 
