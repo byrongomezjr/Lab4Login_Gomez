57d103d39c25f4a4bee7a708b016ea48d26d4304
Byron Gomez Jr <byrongomezjr@protonmail.com>
2023-04-10 19:25:52 -0400

    lab is finalized, functions are working fine.

--- Lab4Login_Gomez/ViewController.swift
+++ Lab4Login_Gomez/ViewController.swift
@@ -11,13 +11,35 @@ class ViewController: UIViewController {
     
     @IBOutlet var usernameTextField: UITextField!
     
-
+    @IBOutlet var forgotUsername: UIButton!
+    
+    @IBOutlet var forgotPassword: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        segue.destination.navigationItem.title = usernameTextField.text
+        guard let sender = sender as? UIButton else { return }
+        
+        if sender == forgotUsername {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else if sender == forgotPassword {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else {
+            segue.destination.navigationItem.title = usernameTextField.text
+        }
     }
+    
+    @IBAction func forgotUsernameButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "forgottenUsernameOrPassword", sender: forgotUsername)
+    }
+    
+    @IBAction func forgotPasswordButton(_ sender: Any) {
+        performSegue(withIdentifier: "forgottenUsernameOrPassword", sender: forgotPassword)
+    }
+    
+    
 }
 
